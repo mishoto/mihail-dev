26134be9128f424ea1b1ef617a93a6181ab94079
mishoto <mihail.dev.io@gmail.com>
2022-08-15 22:01:27 +0300

    create/MD-1.0.0- add icons file

---
+++ src/components/skills/skillsIcons.js
@@ -0,0 +1,70 @@
+import {
+  FaJava,
+  FaJs,
+  FaNodeJs,
+  FaCss3,
+  FaHtml5,
+  FaReact,
+} from 'react-icons/fa';
+import {
+  SiSpringboot,
+  SiWebpack,
+  SiUbuntu,
+  SiApachemaven,
+  SiMysql,
+  SiPostgresql,
+} from 'react-icons/si';
+
+
+const skillIcons = [
+  {
+    id: 1,
+    icon: <FaJava />,
+  },
+  {
+    id: 2,
+    icon: <FaJs />,
+  },
+  {
+    id: 3,
+    icon: <FaNodeJs />,
+  },
+  {
+    id: 4,
+    icon: <FaReact />,
+  },
+  {
+    id: 5,
+    icon: <FaCss3 />,
+  },
+  {
+    id: 6,
+    icon: <FaHtml5 />,
+  },
+  {
+    id: 7,
+    icon: <SiApachemaven />,
+  },
+  {
+    id: 8,
+    icon: <SiPostgresql />,
+  },
+  {
+    id: 9,
+    icon: <SiSpringboot />,
+  },
+  {
+    id: 10,
+    icon: <SiUbuntu />,
+  },
+  {
+    id: 11,
+    icon: <SiWebpack />,
+  },
+  {
+    id: 12,
+    icon: <SiMysql />,
+  },
+];
+
+export default skillIcons;
