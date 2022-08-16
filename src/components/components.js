4d9455be03e849a96d69b9f78ed69b8fb5bbb7e4
mishoto <mihail.dev.io@gmail.com>
2022-08-16 21:35:47 +0300

    update/MD-1.0.0- minor update to imports

--- src/components/components.js
+++ src/components/components.js
@@ -1,4 +1,4 @@
-import Contact from './contact/Contact';
+import ContactContainer from './contact/ContactContainer';
 import Hero from './hero/Hero';
 import Navbar from './navbar/Navbar';
 import ProjectReview from './projectReview/ProjectReview';
@@ -6,4 +6,4 @@ import Resume from './resume/Resume';
 import Skills from './skills/Skills';
 import Error404 from './error/Error404';
 
-export { Contact, Hero, Navbar, ProjectReview, Resume, Skills, Error404 };
+export { ContactContainer, Hero, Navbar, ProjectReview, Resume, Skills, Error404 };
---
+++ src/components/contact/contact.js
@@ -0,0 +1,6 @@
+import ContactContainer from './ContactContainer';
+import ContactForm from './ContactForm';
+import ContactImage from './ContactImage';
+import FormSuccess from './FormSuccess';
+
+export { ContactContainer, ContactForm, ContactImage, FormSuccess };
