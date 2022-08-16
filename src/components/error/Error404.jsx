540c4f1694a83177a20c361d2463220907bcd178
mishoto <mihail.dev.io@gmail.com>
2022-08-16 10:04:19 +0300

    create/MD-1.0.0- add Error404 & Error page with simple text

---
+++ src/components/error/Error404.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Error404.module.css'
+
+const Error404 = () => {
+  return (
+    <>
+    <h2 className={style.h2}>Error 404</h2>
+    <h3 className={style.h3}>Sorry, the page you tried cannot be found!</h3>
+    </>
+  )
+}
+
+export default Error404
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Error404 } from '../../components/components';
+import style from './Error.module.css';
 
 const Error = () => {
   return (
-    <div>Error</div>
+  <section className={style.error_section}>
+    <Error404/>
+    </section>
   )
-}
+};
 
-export default Error
+export default Error;
