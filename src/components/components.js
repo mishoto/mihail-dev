136eb3617b20f10cdd4f704caeeb7c46bf34fa51
mishoto <mihail.dev.io@gmail.com>
2022-08-16 10:07:48 +0300

    update/MD-1.0.0- add all recent components

--- src/components/components.js
+++ src/components/components.js
@@ -1,4 +1,9 @@
+import Contact from './contact/Contact';
+import Hero from './hero/Hero';
+import Navbar from './navbar/Navbar';
+import ProjectReview from './projectReview/ProjectReview';
+import Resume from './resume/Resume';
+import Skills from './skills/Skills';
+import Error404 from './error/Error404';
 
-import Navbar from "./navbar/Navbar";
-
-export {Navbar};
+export { Contact, Hero, Navbar, ProjectReview, Resume, Skills, Error404 };
